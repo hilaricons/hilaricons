23437a4a827824591bd449f8b458de1d329b53a3
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-08-24 11:22:00 +0300

    inital comet

---
+++ README.md
@@ -0,0 +1,25 @@
+<center>
+	<img src="/banner.png">
+	<h1>hilaricons</h1>
+</center>
+hilaricons is a specil icon set bcuz uhh
+
+- funny logo
+- we have a gradient + venn rgb diagram icon
+- why not
+
+we hope that you can use hilaricons within ur next projectery.
+
+## build
+to build, simply get these prerequisites:
+
+1. node + npm (as always)
+2. a convertio api key (for ttf conversion)
+3. `npm i` to get the deps
+4. a positive™ fucking™ attitude™ (the build script doesnt work for non good spirited ppl for now (i think))
+
+then just run the `build.js` file.
+keep in mind all config is stored in the `build.js` file, including whether to build a ttf or an svg
+
+## usage
+for now we don't have any solid way to use hilaricons but we are working for a solution, trust us.
---
+++ build.js
@@ -0,0 +1,46 @@
+fFmly = "hilaricons"
+fId = "Hilaricons"
+inp = "hicns"
+out = "dist"
+startCp = 0xF7676
+order = [ // i didnt make alphabetical sorting cuz that could break backwards compat
+	"f7676",
+	"brainfuck",
+	"hiccup",
+	"h",
+	"grad",
+	"rgb",
+	"back",
+	"fwd",
+	"hicn"
+]
+size=[300,300]
+outTtf = true // this is the flag whether to build an svg or a ttf
+
+async function buildHilaricons([,{default:fetch,FormData},fs,path]) {
+	if(Math.random()<=0.25)return console.error("pls get a more positive attitude") // if i delete this nothing works which is foul
+	if(fs.existsSync(out))fs.rmSync(out,{recursive:!0,force:!0})
+	fs.mkdirSync(out)
+	var paths={}
+	order.forEach(function(id){
+		var{groups:{d}}=fs.readFileSync(path.join(inp,id+".svg"),"utf8").match(Reflect.construct(RegExp,["<path.* d=(?<q>[\"'])(?<d>.+)\\k<q>","s"]))
+		paths[id]=require("svgpath")(d).scale(1,-1).translate(0,size[1])+""
+	})
+	var file=`<svg\x20xmlns="http://www.w3.org/2000/svg"><defs><font\x20id="${fId}"\x20horiz-adv-x="${size[0]}"><font-face\x20font-family="${fFmly}"\x20units-per-em="${size[1]}"\x20ascent="${size[1]}"\x20descent="0"/><missing-glyph/>${order.map(function(id,idx){return`<glyph\x20unicode="&#${startCp+idx};"\x20glyph-name="${id}"\x20d="${paths[id]}"/>`}).join("")}</font></defs></svg>`
+	if(!(outTtf))return(fs).writeFileSync(path.join(out,fId+".svg"),file)
+	var stage1 = await (await fetch("https://api.convertio.co/convert", {
+		method: "POST",
+		headers:{"content-type":"application/json"},
+		body:JSON.stringify({apikey:process.env.CONVERTIO_API_KEY,input:"raw",file,filename:fId+".svg",outputformat:"ttf"})
+	})).json()
+	var stage2 = {data:{}}
+	while (stage2.data.step!="finish") {
+		await Reflect.construct(Promise,[function(a){setTimeout(a,76e2)}])
+		stage2 = await (await fetch(`https://api.convertio.co/convert/${stage1.data.id}/status`)).json()
+		if(stage2.status!="ok")return console.error("yo, we got an issue from the \"convertio\" priest, heres what they have to say:",stage2.error)
+	}
+	await((require("util").promisify(require("stream").pipeline))((await fetch(stage2.data.output.url)).body,require("fs").createWriteStream(path.join(out,fId+".ttf"))))
+	console.log("NOTE: we wasted", "~"+(stage2.data.minutes), "minutes talking to this priest called \"convertio\" which converted our svg to ttfism")
+}
+
+Promise.all([import("dotenv/config"),import("node-fetch"),require("fs"),require("path")]).then(buildHilaricons)
